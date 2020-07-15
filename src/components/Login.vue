8fc4c2d4ec292f1f1722e0d64ad04541bbaeb06d
Diego Alvirde Martinez <dalvirdem@uaemex.mx>
2020-07-15 13:11:19 -0500

    Se agrego metodo login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,8 +8,26 @@
   </div>
 </template>
 <script>
+import firebase from "firebase";
 export default {
   name: "Login",
+  data() {
+    return {
+      usuario: "",
+      contrasena: "",
+    };
+  },
+  methods: {
+    login() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.usuario, this.contrasena)
+        .then(
+          (user) => alert("Usuario conectado " + user),
+          (error) => console.error(error)
+        );
+    },
+  },
 };
 </script>
 <style></style>
