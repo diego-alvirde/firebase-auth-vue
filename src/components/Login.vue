46939fe4ab8525110b9fe3e95fbcef77ce06b171
Diego Alvirde Martinez <dalvirdem@uaemex.mx>
2020-07-15 12:58:17 -0500

    Vue router y componente login

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <form>
+      <input type="text" placeholder="Usuario" />
+      <input type="password" placeholder="Contraseña" />
+      <input type="submit" value="Acceder" />
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: "Login",
+};
+</script>
+<style></style>
