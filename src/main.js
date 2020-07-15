b4ca4d5833f2fb28fcf5f9ce94dfb4fb24251f8c
Diego Alvirde Martinez <dalvirdem@uaemex.mx>
2020-07-15 15:11:25 -0500

    Firebase config

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import firebase from "firebase";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged(function(user) {
+  console.log(user);
+  new Vue({
+    router,
+    render: (h) => h(App),
+  }).$mount("#app");
+});
